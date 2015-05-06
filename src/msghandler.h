e80bab670c8cf0fc8723d2993260ccee0c6dfc30
Denis Zdorovtsov <mrtrizer@gmail.com>
2015-05-06 16:13:03 +0300

    Added an ability to set context for message handler.

--- src/msghandler.h
+++ src/msghandler.h
@@ -1,7 +1,7 @@
 #ifndef MSGHANDLER_H
 #define MSGHANDLER_H
 
-#include "assert.h"
+#include <assert.h>
 #include "msgtemplates.h"
 
 class AbstractMsgHandler
@@ -17,13 +17,16 @@ public:
     virtual ~AbstractMsgHandler(){}
 };
 
-template <class MsgType, class MsgRespType>
+template <class MsgType, class MsgRespType, typename Context>
 class MsgHandler:public AbstractMsgHandler
 {
 public:
-    typedef int (*MsgProcFuc)(const char * data, char * resp);
+    typedef int (*MsgProcFunc)(const char * data, char * resp, const Context & context);
+    typedef int (*MsgProcFuncNC)(const char * data, char * resp);
 
-    MsgHandler(MsgProcFuc msgProcFunc):msgProcFunc(msgProcFunc){}
+    MsgHandler(MsgProcFunc msgProcFunc, const Context & context):msgProcFunc((void*)msgProcFunc),context(context),needContext(true){}
+    MsgHandler(MsgProcFuncNC msgProcFunc, const Context & context):msgProcFunc((void*)msgProcFunc),context(context),needContext(false){}
+    MsgHandler(MsgProcFuncNC msgProcFunc):msgProcFunc((void*)msgProcFunc),context(0),needContext(false){}
     ~MsgHandler(){}
     bool isValidCmd(const char *data, unsigned int size) const
     {
@@ -33,17 +36,24 @@ public:
     int procCmd(const char *data, unsigned int size)
     {
         assert(size == MsgType::getSize());
-        return msgProcFunc(data, (char*)&resp);
+        if (needContext)
+            return ((MsgProcFunc)msgProcFunc)(data, (char*)&resp, context);
+        else
+            return ((MsgProcFuncNC)msgProcFunc)(data, (char*)&resp);
     }
     char * getRespData(){return (char *)&resp;}
     unsigned int getRespLen(){return sizeof(resp);}
-    AbstractMsgHandler * newCopy() const {return new MsgHandler<MsgType, MsgRespType>(*this);}
+    AbstractMsgHandler * newCopy() const {return new MsgHandler<MsgType, MsgRespType, Context>(*this);}
 
 private:
-    MsgProcFuc msgProcFunc;
+    void * msgProcFunc;
     MsgRespType resp;
+    Context context;
+    bool needContext;
 };
 
-#define MSG_HANDLER(in,out,respProc) MsgHandler<in, out>(respProc)
+#define NC int
+#define EC 0
+#define MSG_HANDLER(in,out,contextType,...) MsgHandler<in, out, contextType>(__VA_ARGS__)
 
 #endif // MSGHANDLER_H
